2ecc7ff4d79fd585b5d23a7b90923c96d6213d16
Nicholas Ramsay <nicholasramsay@hotmail.com>
2022-06-05 14:41:36 -0700

    number text should appear when user clicks on button

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 class Calculator{
-	constructor(previousOperand,currentOperand){
-		this.previousOperand = previousOperand;
-		this.currentOperand = currentOperand;
+	constructor(previousOperandText,currentOperandText){
+		this.previousOperandText = previousOperandText;
+		this.currentOperandText = currentOperandText;
+		this.clear();
 
 	}
 	clear(){
-		this.currentOperand = '';
+		this.currentOperand1 = '';
 		this.previousOperand = '';
 		this.operation = undefined;
 	}
@@ -15,19 +16,19 @@ class Calculator{
 	}
 
 	appendNumber(number){
-
+		this.currentOperand1 = number;
 	}
 
 	chooseOperation(operation){
 
 	}
 
-	computer(){
+	compute(){
 
 	}
 
 	updateDisplay(){
-
+		this.currentOperandText.innerText = this.currentOperand1
 	}
 
 }
@@ -37,10 +38,10 @@ const operationButtons =document.querySelectorAll('[data-operation]')
 const equalsButtons = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperand = document.querySelector('[data-prev-op]');
-const currentOperand = document.querySelector('[data-current-op]')
+const previousOperandText = document.querySelector('[data-prev-op]');
+const currentOperandText = document.querySelector('[data-current-op]')
 
-const calculator = new Calculator(previousOperand, currentOperand);
+const calculator = new Calculator(previousOperandText, currentOperandText);
 
 numberButtons.forEach(button =>{ 
 	button.addEventListener('click',() =>
